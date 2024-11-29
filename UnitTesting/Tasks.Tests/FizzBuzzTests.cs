399b0462b94efcdf99a4fe5a7af13f979ab383e0
Aliaksandra_Varabyo1 <Aliaksandra_Varabyova@jabil.com>
2024-11-29 19:04:54 +0200

    added test Should_Print_Numbers_1_To_100; initialized FizzBuzz task

---
+++ UnitTesting/Tasks.Tests/FizzBuzzTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Tasks.Tests
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void Should_Print_Numbers_1_To_100()
+        {
+            var fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.PrintNumbers(1, 100);
+
+            for (int i = 1; i <= 100; i++)
+            {
+
+                Assert.Contains(i.ToString(), result);
+            }
+        }
+
+    }
+}
---
+++ UnitTesting/Tasks/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tasks
+{
+    public class FizzBuzz
+    {
+        public List<string> PrintNumbers(int start, int end)
+        {
+            var result = new List<string>();
+
+            return result;
+        }
+    }
+}
+
